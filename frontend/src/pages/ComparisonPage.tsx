654a00aaca6a50f99b4007db699ab9d92361b19a
Lauren Fries <lauren.e.fries@hotmail.com>
2025-10-28 13:39:55 -0400

    comparison page changes

--- frontend/src/pages/ComparisonPage.tsx
+++ frontend/src/pages/ComparisonPage.tsx
@@ -10,10 +10,28 @@ interface Props {
 function ComparisonPage({ teamOne, teamTwo }: Props) {
   const possibleCombinations = 0; // Placeholder for actual calculation
 
+  if (!teamTwo) {
+    //Display single team
+    return (
+      <>
+        <SingleTeamDisplay
+          team={teamOne}
+          possibleCombinations={possibleCombinations}
+        />
+        <Combination ranks={[1, 2, 3, 4, 5]} />
+      </>
+    );
+  }
+
   return (
+    // Display both teams
     <>
       <SingleTeamDisplay
         team={teamOne}
+        possibleCombinations={possibleCombinations}
+      />
+      <SingleTeamDisplay
+        team={teamTwo}
         possibleCombinations={possibleCombinations}
       />
       <Combination ranks={[1, 2, 3, 4, 5]} />
