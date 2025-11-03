507018c3a7ad05b4f13d7c82b9001c46376a0206
Riley Tate <tater02@pfw.edu>
2025-11-03 16:15:18 -0500

    add footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import styles from "../style/NavBar.module.css";
+
+function Footer() {
+    // Create a way to scroll to the top of the screen
+    const scrollToTop = () => {
+        window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+        });
+
+    };
+
+    return (
+        <>
+        <footer>
+            <button className="button_light" onClick={scrollToTop}>Back to Top</button>
+        </footer>
+        </> 
+    )
+}
+
+export default Footer;
--- frontend/src/components/RankingsPreview.tsx
+++ frontend/src/components/RankingsPreview.tsx
@@ -5,12 +5,12 @@ function RankingsPreview() {
 
     return (
         <>
-            <h2 id="team-rankings-header">Team Rankings (Top 3)</h2>
-            <section className="horizontal-button-section">
+            <h2 id="team-rankings-header">Team Rankings</h2>
+            {/* <section className="horizontal-button-section">
                 <button className="button_dark">3 Teams</button>
                 <button className="button_dark">5 Teams</button>
                 <button className="button_dark">All Teams</button>
-            </section>
+            </section> */}
             <section className="table-section">
                 <RankingsTable />
             </section>
