d15add96592657943046f2b128f71d67c0e04cba
LaurenIsLearning <pleasecontactlauren@gmail.com>
2025-11-20 16:22:23 -0500

    updated with stats and login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ComparisonPage from "./pages/ComparisonPage";
 import AdminPage from "./pages/AdminPage";
+import StatsPage from './pages/StatsPage.tsx'
+import LoginPage from './pages/LoginPage.tsx'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/comparison" element={<ComparisonPage />} />
-      <Route path="/admin" element={<AdminPage />} />
+      <Route path="/Comparison" element={<ComparisonPage />} />
+      <Route path="/Admin" element={<AdminPage />} />
+      <Route path="/Stats" element={<StatsPage teamId={3 /*Placeholder*/}/>}/>
+      <Route path="/Login" element={<LoginPage />}/>
     </Routes>
   );
 }
