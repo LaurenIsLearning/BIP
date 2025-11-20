a82c0ba91ccbfe020459ccbfbfc0d38288ea9d07
Lauren Fries <lauren.e.fries@hotmail.com>
2025-11-20 15:55:28 -0500

    connecting stats page to server to pull player data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,7 @@
 import "./App.css";
-import { Player } from "./models/Player";
-import { Team } from "./models/Team";
-import { useState } from "react";
-import ComparisonPage from "./pages/ComparisonPage";
 
 function App() {
-  return (
-    <>
-      <ComparisonPage teamOne={teamOne} teamTwo={teamTwo} />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- frontend/src/pages/StatsPage.tsx
+++ frontend/src/pages/StatsPage.tsx
@@ -9,35 +9,39 @@ import { useEffect, useState } from "react";
 import PlayerCardSmall from "../components/PlayerCardSmall";
 import NavBar from "../components/NavBar";
 
-function StatsPage() {
+interface Props {
+  teamId: number;
+}
+
+function StatsPage({ teamId }: Props) {
   const [team, setTeam] = useState<Team>();
-  const teamNumber = 3; // Placeholder for team number
 
   useEffect(() => {
     // Rank Placeholder
     let index = 1;
 
     // Fetch team data from the backend
-    fetch(`http://localhost:3000/api/team/${teamNumber}`)
+    fetch(`http://localhost:3000/api/teams/${teamId}`)
       .then((response) => response.json())
       .then((data) => {
         const fetchedTeam = new Team(
-          data.teamName,
+          data.name, // team name
           data.players.map(
-            (p: Player) =>
+            (p: any) =>
               new Player(
-                p.playerName,
-                p.skillLevel,
-                p.sessionWR,
-                p.sessionPA,
-                p.overallWR,
-                p.overallMP,
+                p.name, // player's name
+                p.skill, // skill level
+                p.sesswr, // session win rate
+                p.sesspa, // session points average
+                p.overallwr, // overall win rate
+                p.overallmp, // overall match points
                 false
               )
           ),
-          data.sessionPoints,
+          data.points, // team session points
           data.ranking || index++
         );
+
         setTeam(fetchedTeam);
         setPlayers(fetchedTeam.players);
       });
