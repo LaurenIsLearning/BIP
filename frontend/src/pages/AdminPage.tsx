ece960f1f694e8516c824076c5f5fbc78b106511
LaurenIsLearning <pleasecontactlauren@gmail.com>
2025-11-25 12:30:40 -0500

    adminpage function

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -1,18 +1,15 @@
 import NavBar from "../components/NavBar";
-import HomeHero from "../components/HomeHero";
-import RankingsPreview from "../components/RankingsPreview";
 import "../style/HomePage.css"
 import Footer from "../components/Footer";
 
-function HomePage() {
+function AdminPage() {
     return (
         <>
             <NavBar />
-            {/* <HomeHero /> */}
-            {/* <RankingsPreview /> */}
+
             <Footer />
         </>
     )
 }
 
-export default HomePage;
+export default AdminPage;
