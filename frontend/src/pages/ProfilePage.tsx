1e0b524e34c9f4dae0b2c3cc23fd21e066fffdd8
Riley Tate <tater02@pfw.edu>
2025-11-25 16:15:45 -0500

    getting styling setup

---
+++ frontend/src/pages/ProfilePage.tsx
@@ -0,0 +1,16 @@
+import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
+import ProfileCard from "../components/ProfileCard";
+
+function ProfilePage () {
+
+    return (
+        <>
+        <NavBar />
+        <ProfileCard />
+        <Footer />
+        </>
+    )
+}
+
+export default ProfilePage;
