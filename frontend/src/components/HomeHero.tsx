2b63e0a1207b2371c623786e1d73569d74d1d8f2
Riley Tate <tater02@pfw.edu>
2025-11-25 15:05:45 -0500

    add background image to homepage

--- frontend/src/components/HomeHero.tsx
+++ frontend/src/components/HomeHero.tsx
@@ -2,12 +2,15 @@ function HomeHero() {
     return (
         <>
         <section className="hero">
-            <h1><b>B</b>illards <b>I</b>nformation <b>P</b>rovider</h1>
-            <ul>
-                <li><h3>View Rankings</h3></li>
-                <li><h3>Find Teams</h3></li>
-                <li><h3>Compare Lineups</h3></li>
-            </ul>
+            <section className="overlay"></section>
+            <section className="hero_text">
+                <h1><b>B</b>illards <b>I</b>nformation <b>P</b>rovider</h1>
+                <ul>
+                    <li><h3>View Rankings</h3></li>
+                    <li><h3>Find Teams</h3></li>
+                    <li><h3>Compare Lineups</h3></li>
+                </ul>
+            </section> 
         </section>
         </>
     )
