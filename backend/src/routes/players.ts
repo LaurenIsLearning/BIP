a7d99dc94930681d7bc66a88350adc6a8b60cbcf
LaurenIsLearning <pleasecontactlauren@gmail.com>
2025-12-09 04:02:32 -0500

    players API

--- backend/src/routes/players.ts
+++ backend/src/routes/players.ts
@@ -4,7 +4,7 @@ import { pool } from "../db.js";
 const router = express.Router();
 
 // get all players
-router.get("/all", async (req, res) => {
+router.get("/", async (req, res) => {
     try {
         const result = await pool.query(`
             SELECT 
